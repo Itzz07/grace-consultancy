9d0a0ceacba2b44ac2fd9e79d16a4782cac088e7
itzz07 <zimbajoel1@gmail.com>
2024-07-18 15:04:48 +0200

    commit

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <footer className="bg-gray-800 text-white p-4">
       <div className="max-w-7xl mx-auto text-center">
-        &copy; {new Date().getFullYear()} Consultancy Name. All rights reserved.
+        &copy; {new Date().getFullYear()} Klusaaccounting Services. All rights reserved.
       </div>
     </footer>
   );
